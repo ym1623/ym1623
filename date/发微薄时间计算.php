1a70b8cb42353db336164e4dc5392c0da4299734
ym <598207893@qq.com>
2012-05-30 01:58:16 -0700

    date update

--- date/发微薄时间计算.php
+++ date/发微薄时间计算.php
@@ -17,3 +17,25 @@
 	    }
 	}
 	echo getDateStyle1(strtotime('2012-05-30 12:02:03'));exit;
+
+	function __time($time){
+		$t=time()-$time;
+		$f=array(
+			'31536000'=>'年',
+			'2592000'=>'个月',
+			'604800'=>'星期',
+			'86400'=>'天',
+			'3600'=>'小时',
+			'60'=>'分钟',
+			'1'=>'秒'
+		);
+		foreach ($f as $k=>$v)
+			if (0!=$c=floor($t/(int)$k)){
+				$m = floor($t%$k);
+				foreach($f as $x=>$y)
+					if (0!=$r=floor($m/(int)$x))
+					 return $c.$v.$r.$y.'前';
+				return $c.$v.'前';
+		}
+	}
+	echo __time(time()-61);
