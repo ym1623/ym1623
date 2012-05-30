888d64a216aad07c7e1607268488088e0825efbe
ym <598207893@qq.com>
2012-05-29 21:14:31 -0700

    发微博时间计算

---
+++ date/发微薄时间计算.php
@@ -0,0 +1,19 @@
+<?php
+	function getDateStyle1($time){
+	    $t=time()-$time;
+	    $f=array(
+	        '31536000'=>'年',
+	        '2592000'=>'个月',
+	        '604800'=>'星期',
+	        '86400'=>'天',
+	        '3600'=>'小时',
+	        '60'=>'分钟',
+	        '1'=>'秒'
+	    );
+	    foreach ($f as $k=>$v)    {        
+	        if (0 !=$c=floor($t/(int)$k)) {
+	            return $c.$v.'前';
+	        }
+	    }
+	}
+	echo getDateStyle1(strtotime('2012-05-30 12:02:03'));exit;
